9b1ae137b1f395ee3bf7e6743d4625d55f73814b
Manthan Ugemuge <87055332+ManthanUgemuge@users.noreply.github.com>
2022-03-07 23:25:19 +0530

    Update donut.py

--- donut.py
+++ donut.py
@@ -10,4 +10,17 @@ def main():
     #height=int(input("Enter Screen Height : "))
     #width=int(input("Enter Screen Width : "))
     
+	# for clearing console (windows and unix systems)
+    clear = "cls"
+    if os.name == "posix":
+        clear = "clear"
+
+    os.system(clear)
+    while True:
+        z = [0 for _ in range(4*height*width)]
+        screen = [' ' for _ in range(height*width)]
+
+        j=0
+        
+    
 	
