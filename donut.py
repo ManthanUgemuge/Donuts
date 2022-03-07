59b49f6befc63561ed1768f052afe5a09ffeee16
Manthan Ugemuge <87055332+ManthanUgemuge@users.noreply.github.com>
2022-03-07 23:29:49 +0530

    Update donut.py

--- donut.py
+++ donut.py
@@ -68,17 +68,7 @@ def main():
         b+=0.02
 
 if __name__ == "__main__":
-    main()import os
-from math import sin, cos
-
-def main():
-    a=0
-    b=0
-
-    height=24
-    width=80
-    #height=int(input("Enter Screen Height : "))
-    #width=int(input("Enter Screen Width : "))
+    main()
 
         
 
