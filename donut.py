cf478bd8a8a16c064b837c2fb10cf9f15ec30f2d
Manthan Ugemuge <87055332+ManthanUgemuge@users.noreply.github.com>
2022-03-07 23:24:42 +0530

    Update donut.py

--- donut.py
+++ donut.py
@@ -1,1 +1,14 @@
 
+import os
+from math import sin, cos
+
+def main():
+    a=0
+    b=0
+
+    height=24
+    width=80
+    #height=int(input("Enter Screen Height : "))
+    #width=int(input("Enter Screen Width : "))
+    
+	
